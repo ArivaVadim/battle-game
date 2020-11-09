635132fc027083ed092ffb888b1aeed41c19aa76
Vadim <vadimkacurin71@gmail.com>
2020-11-09 08:24:56 +0300

    Add GLAD library

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("E:/DOCUMENTS/battle-game/build/external/glfw/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("E:/DOCUMENTS/battle-game/build/external/glad/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
